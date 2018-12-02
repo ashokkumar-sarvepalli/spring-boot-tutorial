5d6914c1b7cf59847d831c2d4a40a690b8be3c9e
ASHOKKUMAR SARVEPALLI <ashok@ASHOKKUMARs-MBP.home>
2018-12-02 15:56:57 -0500

    maven

--- component/src/main/java/learning/spring/model/TextEditor.java
+++ component/src/main/java/learning/spring/model/TextEditor.java
@@ -1,22 +1,31 @@
 package learning.spring.model;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class TextEditor {
-	
-	
+
+	@Value("${comp.username}")
+	private String userName;
+	@Value("${comp.password}")
+	private String password;
+
 	@Autowired
 	private WindowsBasedSpellChecker spellChecker;
-	
+
 	public TextEditor(WindowsBasedSpellChecker spellChecker) {
 		this.spellChecker = spellChecker;
 	}
-	
-	
+
 	public void spellCheck() {
-		spellChecker.checkSpelling();
+
+		if (userName.equalsIgnoreCase("admin") && password.equalsIgnoreCase("password")) {
+			spellChecker.checkSpelling();
+		}else {
+			System.out.println("wrong details");
+		}
 	}
 
 }
---
+++ parent/child/src/main/java/org/child/App.java
@@ -0,0 +1,16 @@
+package org.child;
+
+import org.apache.commons.lang3.StringUtils;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "My First maven" );
+        System.out.println(StringUtils.isAllLowerCase("afdsfasdf"));
+    }
+}
